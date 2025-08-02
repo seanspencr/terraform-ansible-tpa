5dd902d465716eb369f3c1f653562dfb75083d9d
seanspencr <sean280806@gmail.com>
2025-08-02 07:56:03 +0700

    initialize

---
+++ .terraform.lock.hcl
@@ -0,0 +1,43 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.5.3"
+  hashes = [
+    "h1:xb77x0HwwHCexdX4nLf5SrknvPskapmi4i1Vk5Tni1M=",
+    "zh:284d4b5b572eacd456e605e94372f740f6de27b71b4e1fd49b63745d8ecd4927",
+    "zh:40d9dfc9c549e406b5aab73c023aa485633c1b6b730c933d7bcc2fa67fd1ae6e",
+    "zh:6243509bb208656eb9dc17d3c525c89acdd27f08def427a0dce22d5db90a4c8b",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:885d85869f927853b6fe330e235cd03c337ac3b933b0d9ae827ec32fa1fdcdbf",
+    "zh:bab66af51039bdfcccf85b25fe562cbba2f54f6b3812202f4873ade834ec201d",
+    "zh:c505ff1bf9442a889ac7dca3ac05a8ee6f852e0118dd9a61796a2f6ff4837f09",
+    "zh:d36c0b5770841ddb6eaf0499ba3de48e5d4fc99f4829b6ab66b0fab59b1aaf4f",
+    "zh:ddb6a407c7f3ec63efb4dad5f948b54f7f4434ee1a2607a49680d494b1776fe1",
+    "zh:e0dafdd4500bec23d3ff221e3a9b60621c5273e5df867bc59ef6b7e41f5c91f6",
+    "zh:ece8742fd2882a8fc9d6efd20e2590010d43db386b920b2a9c220cfecc18de47",
+    "zh:f4c6b3eb8f39105004cf720e202f04f57e3578441cfb76ca27611139bc116a82",
+  ]
+}
+
+provider "registry.terraform.io/telmate/proxmox" {
+  version     = "3.0.1-rc3"
+  constraints = "3.0.1-rc3"
+  hashes = [
+    "h1:kd0CSR9Mbhg6t/Gv4yeIgkbDG2QEwlMuBAxghZhGOd0=",
+    "zh:3699c41289c6fbe0f33b6c54360d43dcfba429de5fbf49506df9276d03aea915",
+    "zh:486c9ddda427d3fecdc6dfa189fce85c4a2aa1f490b024d636c0ac6a4dd3c692",
+    "zh:6091e141a0b8dcb1632c31e0f9555117bb023176c5d083f0e03441bbcf673a4e",
+    "zh:63d312c2c2994ed39dcb47b4d43c89990bd5fff20dbda63cddfb11c9202270f4",
+    "zh:6e69c70a85cfa720f543090ee3ce7d2eb2902df19657121b8b7ae64d44875d9f",
+    "zh:897b9f6075262fc9533f87d470217b14ae82614c6818a26b578a6d41c403d4eb",
+    "zh:91c24bd374fb8ee0c9e4e1c213d157139c047be78b0cafac3c4c9724db8083b0",
+    "zh:a224b58759314dc045fdbfc88b63b036b8ca6f75ad32606e94b553f150077c13",
+    "zh:a56e940c71b45e222c69a2a45388b58ed319836b922f84f62bded5b063662f4a",
+    "zh:b2e0a83aa535cd3493fbc7485d05d1a823c48bf487e313703f01a17edc631908",
+    "zh:ba0ad4fea8ba3b01c67fb164ed92fa927ac70d2d898378d192a01e818fcf6bee",
+    "zh:c49ebe13e7011d35d72e8e6a720df83f21c106444ef4383c5d6c0015aee55db6",
+    "zh:c53e2775040e103aedcce06b9acb79ca5fccdb4c578a4b6e32489c89e9c652dc",
+    "zh:c9002cc470ccfd8cd298d5655cf76af84b1d8a200207973d9ad80235818e89e3",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,183 @@
+
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+
+provider "proxmox" {
+  # Configuration options
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure = true #karena ssl nya masi blm sedcure (http)
+}
+
+
+
+resource "proxmox_vm_qemu" "k8s-master" {
+  name        = "k8s-master"
+  target_node = "pve"
+  vmid       = 300
+  clone      = "ubuntu-jammy2"
+  full_clone = true
+
+  ciuser    = var.ci_user
+  cipassword = var.ci_password
+  sshkeys   = file(var.ci_ssh_public_key)
+
+  agent     = 1
+  cores     = 2
+  memory    = 4096
+  os_type   = "cloud-init"
+  bootdisk  = "scsi0"
+  scsihw    = "virtio-scsi-pci"
+
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = 20
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  boot     = "order=scsi0"
+  ipconfig0 = "ip=192.168.61.169/24,gw=192.168.61.131" #gatewaynya ip proxmox
+  # ipconfig0 = "ip=dhcp"
+  
+  lifecycle {
+    ignore_changes = [ 
+      network
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "k8s-workers" {
+  count       = var.vm_count
+  name        = "k8s-worker-${count.index + 1}"
+  target_node = "pve"
+  vmid        = 301 + count.index
+  clone       = "ubuntu-jammy2"
+  full_clone  = true
+
+  ciuser    = var.ci_user
+  cipassword = var.ci_password
+  sshkeys   = file(var.ci_ssh_public_key)
+
+  agent     = 1
+  cores     = 2
+  memory    = 4096
+  os_type   = "cloud-init"
+  bootdisk  = "scsi0"
+  scsihw    = "virtio-scsi-pci"
+
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size    = 10
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  boot     = "order=scsi0"
+  ipconfig0 = "ip=192.168.61.${count.index + 170}/24,gw=192.168.61.131"
+  # ipconfig0 = "ip=dhcp"
+  
+  lifecycle {
+    ignore_changes = [ 
+      network
+    ]
+  }
+}
+
+output "vm_info" {
+  value = {
+    master = {
+      hostname = proxmox_vm_qemu.k8s-master.name
+      ip_addr  = proxmox_vm_qemu.k8s-master.default_ipv4_address
+    },
+    workers = [
+      for vm in proxmox_vm_qemu.k8s-workers : {
+        hostname = vm.name
+        ip_addr  = vm.default_ipv4_address
+      }
+    ]
+  }
+}
+
+# setelah kelar provisioning, simpen ip addressnya di dlm inventory
+resource "local_file" "create_ansible_inventory" {
+  depends_on = [
+    proxmox_vm_qemu.k8s-master,
+    proxmox_vm_qemu.k8s-workers
+  ]
+
+  content = <<EOT
+[master-node]
+${proxmox_vm_qemu.k8s-master.default_ipv4_address}
+[worker-node]
+${join("\n", [for worker in proxmox_vm_qemu.k8s-workers : worker.default_ipv4_address])}
+
+EOT
+
+  filename = "./inventory.ini"
+}
+
+resource "null_resource" "testing_ansible" {
+  depends_on = [ local_file.create_ansible_inventory ]
+  provisioner "local-exec" {
+    command = "sleep 180;ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory.ini init.yml -u ${var.ci_user} --private-key=${var.ci_ssh_private_key}"
+  }
+}
+
+# resource "null_resource" "install_software" {
+#   depends_on = [ local_file.create_ansible_inventory ]
+#   provisioner "local-exec" {
+#     command = "sleep 180;ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory.ini playbook-install-software.yml -u ${var.ci_user} --private-key=${var.ci_ssh_private_key}"
+#   }
+# }
+
+
+resource "null_resource" "ansible_playbook" {
+    depends_on = [null_resource.testing_ansible]
+    provisioner "local-exec" {
+        command = "sleep 180;ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./inventory.ini playbook-create-k8s-cluster.yml -u ${var.ci_user} --private-key=${var.ci_ssh_private_key}"
+    }
+}
+
+
+
